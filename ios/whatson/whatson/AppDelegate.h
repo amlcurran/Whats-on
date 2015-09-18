e343f8900910526d14b904d763259d169671851f
Alex Curran <aml.curran@gmail.com>
2015-09-18 23:38:19 +0100

    Adds Obj-C ios project

---
+++ ios/whatson/whatson/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  whatson
+//
+//  Created by Alex on 18/09/2015.
+//  Copyright © 2015 Alex Curran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios/whatson/whatson/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  whatson
+//
+//  Created by Alex on 18/09/2015.
+//  Copyright © 2015 Alex Curran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
