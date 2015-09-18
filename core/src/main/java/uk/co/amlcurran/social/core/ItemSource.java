54c6cd1d46a3f3e78a7c0d6b531869b2033255b7
Alex Curran <aml.curran@gmail.com>
2015-09-18 21:20:13 +0100

    Removes unused classes

--- core/src/main/java/uk/co/amlcurran/social/core/ItemSource.java
+++
@@ -1,9 +0,0 @@
-package uk.co.amlcurran.social.core;
-
-public interface ItemSource<T> {
-
-    int count();
-
-    T itemAt(int position);
-
-}
--- core/src/main/java/uk/co/amlcurran/social/core/MyClass.java
+++
@@ -1,4 +0,0 @@
-package uk.co.amlcurran.social.core;
-
-public class MyClass {
-}
