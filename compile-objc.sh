414887bae0c161a722116ed812679ce0b12cd1cf
Alex Curran <aml.curran@gmail.com>
2015-09-19 00:25:40 +0100

    Make compile script hardcode fewer paths

--- compile-objc.sh
+++ compile-objc.sh
@@ -1,17 +1,22 @@
 mkdir -p build/ios/objc
 echo '...transpilin'
-cd build/ios/objc
-j2objc -d . -sourcepath ../../../core/src/main/java/ `find ../../../core/src/main/java/ -name '*.java'`
+j2objc -d build/ios/objc -sourcepath core/src/main/java/ `find core/src/main/java/ -name '*.java'`
 echo
+
 echo '...compilin'
+pushd build/ios/objc > /dev/null
 j2objcc -c -I. `find . -name '*.m'`
+popd > /dev/null
 echo
+
 echo '...static filin'
-libtool -static -o libWhatsOnCore.a `find . -name '*.o'`
+libtool -static -o build/ios/objc/libWhatsOnCore.a `find build/ios/objc -name '*.o'`
 echo
+
 echo '...Copying headers'
-rm -rf ../include
-mkdir ../include
-for FILE in `find . -name '*.h'`; do cp $FILE ../include; done
+rm -rf build/ios/include
+mkdir build/ios/include
+for FILE in `find build/ios/objc -name '*.h'`; do cp $FILE build/ios/include; done
 echo
+
 echo 'Done!'
