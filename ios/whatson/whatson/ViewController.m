fdbf06cbcd8eba9cbe226ebee3762ee3557c6f47
Alex Curran <aml.curran@gmail.com>
2015-09-30 20:49:00 +0100

    Day colour is lighter

--- ios/whatson/whatson/ViewController.m
+++ ios/whatson/whatson/ViewController.m
@@ -21,6 +21,7 @@
 @property EKEventStore *eventStore;
 @property SCCalendarSource *calendarSource;
 @property (nonatomic, strong) NSDateFormatter *formatter;
+@property (nonatomic, strong) UIColor *dayColor;
 
 @end
 
@@ -31,6 +32,7 @@
     self.formatter = [[NSDateFormatter alloc] init];
     self.formatter.dateFormat = @"EEE";
     self.eventStore = [[EKEventStore alloc] init];
+    self.dayColor = [[UIColor blackColor] colorWithAlphaComponent:0.54];
 
     self.navigationController.navigationBar.barTintColor = [UIColor colorWithRed:0.0 green:0.561 blue:0.486 alpha:1];
     self.navigationController.navigationBar.tintColor = [UIColor colorWithWhite:1 alpha:0.8];
@@ -81,7 +83,11 @@
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"day" forIndexPath:indexPath];
     id<SCCalendarItem> item = [self.calendarSource itemAtWithInt:indexPath.row];
     cell.selectionStyle = UITableViewCellSelectionStyleNone;
-    [cell textLabel].text = [NSString stringWithFormat:@"%@: %@", [self.formatter stringFromDate:[NSDate dateWithTimeIntervalSince1970:(item.startTime / 1000)]], [item title]];
+    NSString *threeLetterDay = [self.formatter stringFromDate:[NSDate dateWithTimeIntervalSince1970:(item.startTime / 1000)]];
+    NSString *formatted = [NSString stringWithFormat:@"%@ - %@", threeLetterDay, [item title]];
+    NSMutableAttributedString *coloured = [[NSMutableAttributedString alloc] initWithString:formatted];
+    [coloured addAttribute:NSForegroundColorAttributeName value:self.dayColor range:NSMakeRange(0, 3)];
+    [[cell textLabel] setAttributedText:coloured];
     return cell;
 }
 
