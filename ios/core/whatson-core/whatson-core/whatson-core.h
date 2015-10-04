415cfe4998da13a50c752abfcd3b7d0be1344020
Alex Curran <aml.curran@gmail.com>
2015-10-04 22:41:29 +0100

    Add core for iOS

---
+++ ios/core/whatson-core/whatson-core/whatson-core.h
@@ -0,0 +1,19 @@
+//
+//  whatson-core.h
+//  whatson-core
+//
+//  Created by Alex on 04/10/2015.
+//  Copyright © 2015 Alex Curran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for whatson-core.
+FOUNDATION_EXPORT double whatson_coreVersionNumber;
+
+//! Project version string for whatson-core.
+FOUNDATION_EXPORT const unsigned char whatson_coreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <whatson_core/PublicHeader.h>
+
+
