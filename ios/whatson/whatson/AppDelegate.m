02ba821a1f16cbd08aeaad669cf10bc9654cf387
Alex Curran <aml.curran@gmail.com>
2015-10-04 20:38:31 +0100

    Add today shortcut

--- ios/whatson/whatson/AppDelegate.m
+++ ios/whatson/whatson/AppDelegate.m
@@ -7,39 +7,66 @@
 //
 
 #import "AppDelegate.h"
+#import <EventKit/EventKit.h>
 
 @import EventKitUI;
 
-@interface AppDelegate ()
+@interface AppDelegate () <EKEventEditViewDelegate>
 
 @end
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    NSMutableArray <UIApplicationShortcutItem *> *updatedShortcuts = [[NSMutableArray alloc] init];
     
-    NSArray <UIApplicationShortcutItem *> *currentShortcuts = application.shortcutItems;
-    if ([currentShortcuts count] == 0) {
-        NSMutableArray <UIApplicationShortcutItem *> *updatedShortcuts = [[NSMutableArray alloc] init];
-        UIMutableApplicationShortcutItem *newItem = [[UIMutableApplicationShortcutItem alloc] initWithType:@"new-tomorrow" localizedTitle:@"Add event tomorrow"];
-        [updatedShortcuts addObject:newItem];
-        [application setShortcutItems:updatedShortcuts];
-    }
+    UIMutableApplicationShortcutItem *newItem = [[UIMutableApplicationShortcutItem alloc] initWithType:@"new-tomorrow" localizedTitle:@"Add event tomorrow"];
+    [newItem setIcon:[UIApplicationShortcutIcon iconWithType:UIApplicationShortcutIconTypeAdd]];
+    UIMutableApplicationShortcutItem *onToday = [[UIMutableApplicationShortcutItem alloc] initWithType:@"on-today" localizedTitle:@"On today"];
+    [onToday setIcon:[UIApplicationShortcutIcon iconWithType:UIApplicationShortcutIconTypeDate]];
     
-    if (launchOptions[UIApplicationLaunchOptionsShortcutItemKey]) {
+    [updatedShortcuts addObject:onToday];
+    [updatedShortcuts addObject:newItem];
+    
+    [application setShortcutItems:updatedShortcuts];
+    
+    UIApplicationShortcutItem *item = launchOptions[UIApplicationLaunchOptionsShortcutItemKey];
+    if ([item.type isEqualToString:@"new-tomorrow"]) {
         EKEventEditViewController *editController = [[EKEventEditViewController alloc] init];
         editController.eventStore = [EKEventStore new];
         [self.window.rootViewController presentViewController:editController animated:YES completion:nil];
         return NO;
+    } else if ([item.type isEqualToString:@"on-today"]) {
+        UIAlertController *controller = [UIAlertController alertControllerWithTitle:@"Uh oh!" message:@"You've not implemented this yet" preferredStyle:UIAlertControllerStyleAlert];
+        [self.window.rootViewController presentViewController:controller animated:YES completion:nil];
+        return YES;
     }
     return YES;
 }
 
 - (void)application:(UIApplication *)application performActionForShortcutItem:(UIApplicationShortcutItem *)shortcutItem completionHandler:(void (^)(BOOL succeeded))completionHandler
 {
-    EKEventEditViewController *editController = [[EKEventEditViewController alloc] init];
-    editController.eventStore = [EKEventStore new];
-    [self.window.rootViewController presentViewController:editController animated:YES completion:nil];
+    [self handleShortcutItem:shortcutItem];
+}
+
+- (void)handleShortcutItem:(UIApplicationShortcutItem *)item
+{
+    if ([item.type isEqualToString:@"new-tomorrow"]) {
+        EKEvent *event = [EKEvent eventWithEventStore:[[EKEventStore alloc] init]];
+        NSDate *date = [[NSCalendar currentCalendar] dateBySettingHour:17 minute:0 second:0 ofDate:[NSDate new] options:0];
+        event.startDate = date;
+        EKEventEditViewController *editController = [[EKEventEditViewController alloc] init];
+        editController.eventStore = [EKEventStore new];
+        [self.window.rootViewController presentViewController:editController animated:YES completion:nil];
+    } else if ([item.type isEqualToString:@"on-today"]) {
+        UIAlertController *controller = [UIAlertController alertControllerWithTitle:@"Uh oh!" message:@"You've not implemented this yet" preferredStyle:UIAlertControllerStyleAlert];
+        [self.window.rootViewController presentViewController:controller animated:YES completion:nil];
+    }
+}
+
+- (void)eventEditViewController:(EKEventEditViewController *)controller didCompleteWithAction:(EKEventEditViewAction)action
+{
+    [self.window.rootViewController dismissViewControllerAnimated:YES completion:nil];
 }
 
 @end
