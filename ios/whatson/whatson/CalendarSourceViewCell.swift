2d843cb81acece6224b56f07f03c50b95016229a
Alex Curran <aml.curran@gmail.com>
2016-02-14 21:40:25 +0000

    Update UI

--- ios/whatson/whatson/CalendarSourceViewCell.swift
+++ ios/whatson/whatson/CalendarSourceViewCell.swift
@@ -13,13 +13,13 @@ class CalendarSourceViewCell : UITableViewCell {
 
     @IBOutlet weak var mainLabel: UILabel!
     @IBOutlet weak var numberLabel: UILabel!
-    @IBOutlet var marginConstraint: NSLayoutConstraint!
-    @IBOutlet var widthConstraint: NSLayoutConstraint!
-    @IBOutlet var mainMarginConstraint: NSLayoutConstraint!
     
     let dateFormatter = NSDateFormatter();
 
     let dayColor = UIColor.blackColor().colorWithAlphaComponent(0.54);
+    @IBOutlet weak var `internal`: UIView!
+    
+    
 
     func bind(item : SCCalendarItem, slot : SCCalendarSlot?) {
         dateFormatter.dateFormat = "EEE";
@@ -34,21 +34,15 @@ class CalendarSourceViewCell : UITableViewCell {
         
 //        self.bounds = CGRectInset(self.frame, 16, 16);
         mainLabel.attributedText = colouredString;
-        self.layer.borderWidth = 2;
-        self.layer.borderColor = UIColor.blackColor().colorWithAlphaComponent(0.10).CGColor;
-        self.layer.cornerRadius = 8;
-        
+//        self.`internal`.layer.borderWidth = 2;
+//        self.`internal`.layer.borderColor = UIColor.blackColor().colorWithAlphaComponent(0.10).CGColor;
+//        self.`internal`.layer.cornerRadius = 8;
+//        
         if (slot != nil && slot!.count() > 1) {
             numberLabel.text = String(format: "+%lu", slot!.count() - 1);
             numberLabel.hidden = false;
-            widthConstraint.active = true;
-            marginConstraint.active = true;
-            mainMarginConstraint.active = false;
         } else {
             numberLabel.hidden = true;
-            widthConstraint.active = false;
-            marginConstraint.active = false;
-            mainMarginConstraint.active = true;
         }
     }
     
---
+++ ios/whatson/whatson/RoundedRectBorderView.swift
@@ -0,0 +1,31 @@
+//
+//  RoundedRectBorderView.swift
+//  whatson
+//
+//  Created by Alex on 14/02/2016.
+//  Copyright © 2016 Alex Curran. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedRectBorderView: UIView {
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+    didSet {
+        layer.cornerRadius = cornerRadius
+        layer.masksToBounds = cornerRadius > 0
+    }
+    }
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    @IBInspectable var borderColor: UIColor? {
+        didSet {
+            layer.borderColor = borderColor?.CGColor
+        }
+    }
+
+}
