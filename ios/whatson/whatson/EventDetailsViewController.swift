5b11b3269fa92e10d2903ba1ec8f353cc4815284
Alex Curran <aml.curran@gmail.com>
2016-07-23 15:41:11 +0200

    Add Geocoding for event places

--- ios/whatson/whatson/EventDetailsViewController.swift
+++ ios/whatson/whatson/EventDetailsViewController.swift
@@ -32,10 +32,34 @@ class EventDetailsViewController: UIViewController {
         timingLabel.text = "From \(event.startDate.formatAsTime()) to \(event.endDate.formatAsTime())"
         
         if let location = event.structuredLocation?.geoLocation {
-            let region = MKCoordinateRegion(center: location.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
-            locationMapView.setRegion(region, animated: false)
+            updateMap(location: location)
         } else {
             mapHeightConstraint.constant = 0
+            if let location = event.location {
+            geocoder.geocodeAddressString(location, completionHandler: { [weak self] (places, error) in
+                if let places = places, let firstPlace = places.first, let location = firstPlace.location {
+                    self?.mapHeightConstraint.constant = 160
+                    UIView.animate(withDuration: 0.2, animations: { 
+                        self?.view.layoutIfNeeded()
+                    })
+                    self?.updateMap(location: location)
+                }
+            })
+            }
+        }
+    }
+    
+    private func updateMap(location: CLLocation) {
+        let region = MKCoordinateRegion(center: location.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        locationMapView.setRegion(region, animated: false)
+        let point = MKPointAnnotation()
+        point.coordinate = location.coordinate
+        locationMapView.addAnnotation(point)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        if (geocoder.isGeocoding) {
+            geocoder.cancelGeocode()
         }
     }
 
