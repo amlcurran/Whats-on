be6deb23fef9717e1a15b7a3f8020e770419fee6
Alex Curran <aml.curran@gmail.com>
2016-10-07 11:27:44 +0200

    Don't show the title in the details page

--- ios/whatson/whatson/WhatsOnViewController.swift
+++ ios/whatson/whatson/WhatsOnViewController.swift
@@ -50,7 +50,7 @@ class WhatsOnViewController: UIViewController, EKEventEditViewDelegate, UIViewCo
         tableView.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor, constant: 0).isActive = true
         tableView.contentInset = UIEdgeInsets(top: header.intrinsicContentSize.height + 38, left: 0, bottom: 16, right: 0)
         
-        title = "What's On";
+        title = " "
         
         let newCellNib = UINib(nibName: "CalendarCell", bundle: Bundle.main)
         tableView.register(newCellNib, forCellReuseIdentifier: "day")
