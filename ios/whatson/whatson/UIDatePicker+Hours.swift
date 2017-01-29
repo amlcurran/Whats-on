13d5f4e10510e05e5973f6b39eda3bbbb1cecbfb
amlcurran <aml.curran@gmail.com>
2017-01-29 14:01:01 +0100

    Move extension to its own file

---
+++ ios/whatson/whatson/UIDatePicker+Hours.swift
@@ -0,0 +1,37 @@
+import Foundation
+
+extension UIDatePicker {
+
+    var hour: Int? {
+        get {
+            return Calendar.current.dateComponents([.hour], from: date).hour
+        }
+    }
+
+    func set(hour: Int, limitedAfter lowerLimit: Int) {
+        var components = Calendar.current.dateComponents([.day, .hour, .minute], from: Date())
+        components.hour = hour
+        components.minute = 0
+        date = Calendar.current.date(from: components)!
+
+        components = Calendar.current.dateComponents([.day, .hour, .minute], from: Date())
+        components.hour = lowerLimit
+        components.minute = 0
+        minimumDate = Calendar.current.date(from: components)
+        maximumDate = nil
+    }
+
+    func set(hour: Int, limitedBefore upperLimit: Int) {
+        var components = Calendar.current.dateComponents([.day, .hour, .minute], from: Date())
+        components.hour = hour
+        components.minute = 0
+        date = Calendar.current.date(from: components)!
+
+        components = Calendar.current.dateComponents([.day, .hour, .minute], from: Date())
+        components.hour = upperLimit
+        components.minute = 0
+        minimumDate = nil
+        maximumDate = Calendar.current.date(from: components)
+    }
+
+}
