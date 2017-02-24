8da7254b2342c54c35913cd82b0746011851b01f
amlcurran <aml.curran@gmail.com>
2017-02-24 22:37:26 +0100

    Modify predicates and hide declined events

--- ios/whatson/whatson/EventPredicates.swift
+++ ios/whatson/whatson/EventPredicates.swift
@@ -5,7 +5,11 @@ struct EventPredicates {
     let timeRepository: SCTimeRepository
 
     func standard() -> NSPredicate {
-        return NSPredicate(compoundFrom: [notAllDay(), isWithinBorder(timeRepository: timeRepository, using: Calendar.autoupdatingCurrent)])
+        return NSPredicate(compoundFrom: [
+                notAllDay(),
+                isWithinBorder(timeRepository: timeRepository, using: Calendar.autoupdatingCurrent),
+                notDeclinedOrCancelled()
+        ])
     }
 
 }
@@ -22,28 +26,41 @@ private extension NSPredicate {
 
 }
 
+private func notDeclinedOrCancelled() -> NSPredicate {
+    return NSPredicate(eventBlock: { event in
+        return event.status != EKEventStatus.canceled
+    })
+}
+
 private func notAllDay() -> NSPredicate {
-    return NSPredicate(block: { (element, _) -> Bool in
-        if let event = element as? EKEvent {
-            return !event.isAllDay
-        }
-        return false
+    return NSPredicate(eventBlock: { event in
+        return !event.isAllDay
     })
 }
 
 private func isWithinBorder(timeRepository: SCTimeRepository, using calendar: Calendar) -> NSPredicate {
-    return NSPredicate(block: { (element, _) -> Bool in
-        if let event = element as? EKEvent {
-            var startComponents = calendar.dateComponents([.hour, .day], from: event.startDate)
-            var endComponents = calendar.dateComponents([.hour, .day], from: event.endDate)
-            startComponents.timeZone = TimeZone.current
-            endComponents.timeZone = TimeZone.current
-            let endHour = endComponents.hour!
-            let startHour = startComponents.hour!
-            let endsBefore = endHour <= Int(timeRepository.borderTimeStart().toHours()) && endComponents.day! == startComponents.day!
-            let beginsAfter = startHour > Int(timeRepository.borderTimeEnd().toHours())
-            return !(endsBefore || beginsAfter)
-        }
-        return false
+    return NSPredicate(eventBlock: { event in
+        var startComponents = calendar.dateComponents([.hour, .day], from: event.startDate)
+        var endComponents = calendar.dateComponents([.hour, .day], from: event.endDate)
+        startComponents.timeZone = TimeZone.current
+        endComponents.timeZone = TimeZone.current
+        let endHour = endComponents.hour!
+        let startHour = startComponents.hour!
+        let endsBefore = endHour <= Int(timeRepository.borderTimeStart().toHours()) && endComponents.day! == startComponents.day!
+        let beginsAfter = startHour > Int(timeRepository.borderTimeEnd().toHours())
+        return !(endsBefore || beginsAfter)
     })
+}
+
+extension NSPredicate {
+
+    convenience init(eventBlock: @escaping ((EKEvent) -> Bool)) {
+        self.init(block: { element, _ in
+            if let event = element as? EKEvent {
+                return eventBlock(event)
+            }
+            return false
+        })
+    }
+
 }
