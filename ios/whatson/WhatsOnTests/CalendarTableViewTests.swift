397d8bb43fb87836ecd3d397cc97d0209d6f18d1
amlcurran <aml.curran@gmail.com>
2017-07-16 17:57:49 +0200

    Add some unit tests around updating rows

---
+++ ios/whatson/WhatsOnTests/CalendarTableViewTests.swift
@@ -0,0 +1,67 @@
+import XCTest
+import UIKit
+@testable import What_s_on
+
+class CalendarTableViewTests: XCTestCase, CalendarTableViewDelegate {
+
+    func testIfTheDiffIsEmptyTheTableViewReloadsCompletely() {
+        let stubTableView = StubTableView()
+        let calendarTableView = CalendarTableView(delegate: self, dataProvider: StubDataProvider(updatedIndexes: []), tableView: stubTableView)
+        let newData = SCCalendarSource()
+
+        calendarTableView.update(newData)
+
+        XCTAssertTrue(stubTableView.calledReloadData)
+    }
+
+    func testIfTheDiffIsNotEmptyTheTableViewReloadsIndividualRowsOfEvents() {
+        let stubTableView = StubTableView()
+        let calendarTableView = CalendarTableView(delegate: self, dataProvider: StubDataProvider(updatedIndexes: [0, 3]), tableView: stubTableView)
+        let newData = SCCalendarSource()
+
+        calendarTableView.update(newData)
+
+        XCTAssertEqual(stubTableView.reloadedRowIndexes, [IndexPath(row: 1, section: 0), IndexPath(row: 7, section: 0)])
+    }
+
+    func addEvent(for item: SCCalendarItem) {
+        // no-op
+    }
+
+    func showDetails(for item: SCEventCalendarItem, at indexPath: IndexPath, in cell: UITableViewCell) {
+        // no-op
+    }
+
+    func remove(_ event: SCEventCalendarItem) {
+        // no-op
+    }
+
+}
+
+class StubDataProvider: DataProvider {
+
+    let updatedIndexes: [Int]
+
+    init(updatedIndexes: [Int]) {
+        self.updatedIndexes = updatedIndexes
+    }
+
+    override func update(from source: SCCalendarSource) -> [Int] {
+        return updatedIndexes
+    }
+
+}
+
+class StubTableView: UITableView {
+
+    var calledReloadData = false
+    var reloadedRowIndexes: [IndexPath] = []
+
+    override func reloadData() {
+        calledReloadData = true
+    }
+
+    override func reloadRows(at indexPaths: [IndexPath], with animation: UITableViewRowAnimation) {
+        reloadedRowIndexes = indexPaths
+    }
+}
