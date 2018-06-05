60730902374302d802eb9616787f673e4c61248f
amlcurran <aml.curran@gmail.com>
2018-06-05 21:51:19 +0200

    Pull out day view from day cell

--- ios/whatson/whatson/day list/DayCell.swift
+++ ios/whatson/whatson/day list/DayCell.swift
@@ -2,19 +2,40 @@ import UIKit
 
 class DayCell: UITableViewCell {
 
-    let dayLabel = UILabel()
-    let dayFormatter = DateFormatter()
+    private let dayView = DayView()
 
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        styleViews()
         layout()
     }
 
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        styleViews()
         layout()
+    }
+
+    private func layout() {
+        contentView.add(dayView, constrainedTo: [.leading, .trailing, .top, .bottom])
+    }
+
+    func bound(to item: SCCalendarItem) -> Self {
+        dayView.bind(to: item)
+        return self
+    }
+
+}
+
+class DayView: UIView {
+
+    private let dayLabel = UILabel()
+    private let dayFormatter = DateFormatter()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 
     private func styleViews() {
@@ -28,15 +49,14 @@ class DayCell: UITableViewCell {
     }
 
     private func layout() {
-        contentView.layoutMargins = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
-        contentView.add(dayLabel, constrainedTo: [.leadingMargin, .trailingMargin], withInset: 8)
-        dayLabel.constrain(.top, to: contentView, .top, withOffset: 16)
-        dayLabel.constrain(.bottom, to: contentView, .bottom, withOffset: -4)
+        layoutMargins = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
+        add(dayLabel, constrainedTo: [.leadingMargin, .trailingMargin], withInset: 8)
+        dayLabel.constrain(.top, to: self, .top, withOffset: 16)
+        dayLabel.constrain(.bottom, to: self, .bottom, withOffset: -4)
     }
 
-    func bound(to item: SCCalendarItem) -> Self {
+    func bind(to item: SCCalendarItem) {
         dayLabel.text = dayFormatter.string(from: Date(from: item.startTime()))
-        return self
     }
 
 }
