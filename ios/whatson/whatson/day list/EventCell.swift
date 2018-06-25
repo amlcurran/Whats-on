a1d6cef2d4acd88f6ae9f0ab746df49ac00ab370
amlcurran <aml.curran@gmail.com>
2018-06-25 22:26:33 +0200

    Pull out event view from the cell

--- ios/whatson/whatson/day list/EventCell.swift
+++ ios/whatson/whatson/day list/EventCell.swift
@@ -4,115 +4,34 @@ import UIKit
 @IBDesignable
 class EventCell: UITableViewCell {
 
-    let eventLabel = UILabel()
-    let roundedView = RoundedRectBorderView()
-    let timeFormatter = DateFormatter.shortTime
-    let timeCalculator = NSDateCalculator.instance
-    let secondaryLabel = UILabel()
-    let otherEventsLabel = UILabel()
+    private let eventView = EventItemView()
 
-    var secondaryLabelZeroHeightConstraint: NSLayoutConstraint?
-    var secondaryLabelBelowConstraint: NSLayoutConstraint?
-
-    var type: SlotCellStyle = .empty {
-        didSet {
-            roundedView.backgroundColor = type.cellBackground
-            eventLabel.textColor = type.mainText
-            secondaryLabel.textColor = type.secondaryText
-            otherEventsLabel.textColor = type.secondaryText
-            roundedView.borderColor = type.borderColor
-            roundedView.borderWidth = type.borderWidth
-            roundedView.borderDash = type.borderDash
-            updateSecondaryHeight()
-        }
-    }
-
-    var detail: SlotDetail = .mid {
-        didSet {
-            updateSecondaryHeight()
-        }
+    var roundedView: RoundedRectBorderView {
+        return eventView.roundedView
     }
 
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        styleViews()
         layout()
+        backgroundColor = .clear
     }
 
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        styleViews()
         layout()
-    }
-
-    private func styleViews() {
-        timeFormatter.dateStyle = .none
-        timeFormatter.timeStyle = .short
         backgroundColor = .clear
-        selectionStyle = .none
-
-        eventLabel.font = UIFont.systemFont(ofSize: 16, weight: UIFont.Weight.semibold)
-        secondaryLabel.font = UIFont.systemFont(ofSize: 14, weight: UIFont.Weight.medium)
-        otherEventsLabel.font = .systemFont(ofSize: 18, weight: UIFont.Weight.medium)
-        otherEventsLabel.textAlignment = .right
     }
 
     private func layout() {
-        roundedView.addSubview(otherEventsLabel)
-        otherEventsLabel.constrain(toSuperview: .trailing, .top, .bottom, insetBy: 16)
-        roundedView.addSubview(eventLabel)
-        eventLabel.constrain(toSuperview: .top, .leading, insetBy: 16)
-        eventLabel.constrain(.trailing, to: otherEventsLabel, .leading)
-        eventLabel.setContentHuggingPriority(UILayoutPriority.required, for: .vertical)
-        roundedView.addSubview(secondaryLabel)
-        secondaryLabel.constrain(toSuperview: .leading, .trailing, .bottom, insetBy: 16)
-        secondaryLabel.setContentHuggingPriority(UILayoutPriority.required, for: .vertical)
-        secondaryLabelBelowConstraint = secondaryLabel.constrain(.top, to: eventLabel, .bottom, withOffset: 4)
-        secondaryLabelZeroHeightConstraint = secondaryLabel.constrain(height: 0)
-        updateSecondaryHeight()
-
-        contentView.layoutMargins = UIEdgeInsets(top: 8, left: 16, bottom: 8, right: 16)
-        contentView.add(roundedView, constrainedTo: [.topMargin, .leadingMargin, .trailingMargin, .bottomMargin])
-        roundedView.cornerRadius = 6
-        roundedView.layer.shadowOffset = CGSize(width: 0, height: 6)
-        roundedView.layer.shadowRadius = 4
-        roundedView.layer.shadowColor = UIColor.red.cgColor
-    }
-
-    func updateSecondaryHeight() {
-        if detail.isSecondaryTextShown && type.isSecondaryTextShown {
-            secondaryLabelZeroHeightConstraint?.isActive = false
-            secondaryLabelBelowConstraint?.constant = 4
-        } else {
-            secondaryLabelZeroHeightConstraint?.isActive = true
-            secondaryLabelBelowConstraint?.constant = 0
-        }
+        contentView.addSubview(eventView)
+        contentView.backgroundColor = .clear
+        selectionStyle = .none
+        eventView.constrain(toSuperview: .leading, .trailing, .top, .bottom)
     }
 
     func bound(to item: SCCalendarItem, slot: SCCalendarSlot) -> Self {
-        let startTime = timeFormatter.string(from: timeCalculator.date(from: item.startTime()))
-        secondaryLabel.text = String(format: "From %@", startTime)
-        if slot.isEmpty() {
-            type = .empty
-            eventLabel.text = "Add event"
-        } else {
-            type = .full
-            eventLabel.text = item.title()
-        }
-        if slot.count() > 1 {
-            otherEventsLabel.text = "+\(slot.extraItemsCount)"
-        } else {
-            otherEventsLabel.text = nil
-        }
+        _ = eventView.bound(to: item, slot: slot)
         return self
-    }
-
-}
-
-extension SCCalendarSlot {
-
-    var extraItemsCount: Int {
-        return Int(count()) - 1
     }
 
 }
---
+++ ios/whatson/whatson/day list/EventItemView.swift
@@ -0,0 +1,123 @@
+//
+//  EventView.swift
+//  whatson
+//
+//  Created by Alex Curran on 25/06/2018.
+//  Copyright © 2018 Alex Curran. All rights reserved.
+//
+
+import UIKit
+
+class EventItemView: UIView {
+
+    let eventLabel = UILabel()
+    let roundedView = RoundedRectBorderView()
+    let timeFormatter = DateFormatter.shortTime
+    let timeCalculator = NSDateCalculator.instance
+    let secondaryLabel = UILabel()
+    let otherEventsLabel = UILabel()
+
+    var secondaryLabelZeroHeightConstraint: NSLayoutConstraint?
+    var secondaryLabelBelowConstraint: NSLayoutConstraint?
+
+    private var type: SlotCellStyle = .empty {
+        didSet {
+            roundedView.backgroundColor = type.cellBackground
+            eventLabel.textColor = type.mainText
+            secondaryLabel.textColor = type.secondaryText
+            otherEventsLabel.textColor = type.secondaryText
+            roundedView.borderColor = type.borderColor
+            roundedView.borderWidth = type.borderWidth
+            roundedView.borderDash = type.borderDash
+            updateSecondaryHeight()
+        }
+    }
+
+    private var detail: SlotDetail = .mid {
+        didSet {
+            updateSecondaryHeight()
+        }
+    }
+
+    init() {
+        super.init(frame: .zero)
+        styleViews()
+        layout()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        styleViews()
+        layout()
+    }
+
+    private func styleViews() {
+        timeFormatter.dateStyle = .none
+        timeFormatter.timeStyle = .short
+        backgroundColor = .clear
+
+        eventLabel.font = UIFont.systemFont(ofSize: 16, weight: UIFont.Weight.semibold)
+        secondaryLabel.font = UIFont.systemFont(ofSize: 14, weight: UIFont.Weight.medium)
+        otherEventsLabel.font = .systemFont(ofSize: 18, weight: UIFont.Weight.medium)
+        otherEventsLabel.textAlignment = .right
+    }
+
+    private func layout() {
+        roundedView.addSubview(otherEventsLabel)
+        otherEventsLabel.constrain(toSuperview: .trailing, .top, .bottom, insetBy: 16)
+        roundedView.addSubview(eventLabel)
+        eventLabel.constrain(toSuperview: .top, .leading, insetBy: 16)
+        eventLabel.constrain(.trailing, to: otherEventsLabel, .leading)
+        eventLabel.setContentHuggingPriority(UILayoutPriority.required, for: .vertical)
+        roundedView.addSubview(secondaryLabel)
+        secondaryLabel.constrain(toSuperview: .leading, .trailing, .bottom, insetBy: 16)
+        secondaryLabel.setContentHuggingPriority(UILayoutPriority.required, for: .vertical)
+        secondaryLabelBelowConstraint = secondaryLabel.constrain(.top, to: eventLabel, .bottom, withOffset: 4)
+        secondaryLabelZeroHeightConstraint = secondaryLabel.constrain(height: 0)
+        updateSecondaryHeight()
+
+        layoutMargins = UIEdgeInsets(top: 8, left: 16, bottom: 8, right: 16)
+        add(roundedView, constrainedTo: [.topMargin, .leadingMargin, .trailingMargin, .bottomMargin])
+        roundedView.cornerRadius = 6
+        roundedView.layer.shadowOffset = CGSize(width: 0, height: 6)
+        roundedView.layer.shadowRadius = 4
+        roundedView.layer.shadowColor = UIColor.red.cgColor
+    }
+
+    func updateSecondaryHeight() {
+        if detail.isSecondaryTextShown && type.isSecondaryTextShown {
+            secondaryLabelZeroHeightConstraint?.isActive = false
+            secondaryLabelBelowConstraint?.constant = 4
+        } else {
+            secondaryLabelZeroHeightConstraint?.isActive = true
+            secondaryLabelBelowConstraint?.constant = 0
+        }
+    }
+
+    func bound(to item: SCCalendarItem, slot: SCCalendarSlot) -> Self {
+        let startTime = timeFormatter.string(from: timeCalculator.date(from: item.startTime()))
+        secondaryLabel.text = String(format: "From %@", startTime)
+        if slot.isEmpty() {
+            type = .empty
+            eventLabel.text = "Add event"
+        } else {
+            type = .full
+            eventLabel.text = item.title()
+        }
+        if slot.count() > 1 {
+            otherEventsLabel.text = "+\(slot.extraItemsCount)"
+        } else {
+            otherEventsLabel.text = nil
+        }
+        return self
+    }
+
+}
+
+private extension SCCalendarSlot {
+
+    var extraItemsCount: Int {
+        return Int(count()) - 1
+    }
+
+}
