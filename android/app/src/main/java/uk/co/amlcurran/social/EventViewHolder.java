bafa936fdc015d200672e79b33847142b8bd9b2b
Alex Curran <alex@novoda.com>
2019-05-17 16:02:13 +0200

    Add time to events view

--- android/app/src/main/java/uk/co/amlcurran/social/EventViewHolder.java
+++ android/app/src/main/java/uk/co/amlcurran/social/EventViewHolder.java
@@ -3,20 +3,28 @@ package uk.co.amlcurran.social;
 import android.view.View;
 import android.widget.TextView;
 
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
 public class EventViewHolder extends CalendarItemViewHolder<EventCalendarItem> {
 
-    private final TextView textView;
+    private final TextView textView;;
     private final WhatsOnAdapter.EventSelectedListener eventSelectedListener;
+    private final TextView subtitle;
+    private final DateTimeFormatter formatter = DateTimeFormat.shortTime();
+    private final JodaCalculator jodaCalculator = new JodaCalculator();
 
     public EventViewHolder(View itemView, WhatsOnAdapter.EventSelectedListener eventSelectedListener) {
         super(itemView);
         this.eventSelectedListener = eventSelectedListener;
         this.textView = itemView.findViewById(R.id.textView);
+        this.subtitle = itemView.findViewById(R.id.event_subtitle);
     }
 
     @Override
     public void bind(final EventCalendarItem item) {
         textView.setText(item.title());
+        subtitle.setText(subtitle.getResources().getString(R.string.event_from, formatter.print(jodaCalculator.getDateTime(item.startTime()))));
         itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
