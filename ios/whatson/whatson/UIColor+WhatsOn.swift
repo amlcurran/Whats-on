ef7b0a395bed64761bee897976dd6adae595c1c0
amlcurran <aml.curran@gmail.com>
2019-07-12 15:31:30 +0200

    Switch to named colours

--- ios/whatson/whatson/UIColor+WhatsOn.swift
+++ ios/whatson/whatson/UIColor+WhatsOn.swift
@@ -2,31 +2,31 @@ import UIKit
 
 extension UIColor {
     class var emptyOutline: UIColor {
-        return UIColor(red: 194.0 / 255.0, green: 207.0 / 255.0, blue: 214.0 / 255.0, alpha: 1.0)
+        return UIColor(named: "emptyOutline")!
     }
 
     class var accent: UIColor {
-        return UIColor(red: 58.0 / 255.0, green: 217.0 / 255.0, blue: 112.0 / 255.0, alpha: 1.0)
+        return UIColor(named: "accent")!
     }
 
     class var secondary: UIColor {
-        return UIColor(red: 68.0 / 255.0, green: 82.0 / 255.0, blue: 108.0 / 255.0, alpha: 1.0)
+        return UIColor(named: "secondary")!
     }
 
     class var lightText: UIColor {
-        return UIColor(red: 186.0 / 255.0, green: 194.0 / 255.0, blue: 199.0 / 255.0, alpha: 1.0)
+        return UIColor(named: "lightText")!
     }
 
     class var windowBackground: UIColor {
-        return UIColor(red: 237.0 / 255.0, green: 241.0 / 255.0, blue: 245.0 / 255.0, alpha: 1.0)
+        return UIColor(named: "windowBackground")!
     }
 
     class var divider: UIColor {
-        return UIColor(red: 226.0 / 255.0, green: 233.0 / 255.0, blue: 238.0 / 255.0, alpha: 1.0)
+        return UIColor(named: "divider")!
     }
 
     class var cardDivider: UIColor {
-        return UIColor(red: 245.0 / 255.0, green: 246.0 / 255.0, blue: 247.0 / 255.0, alpha: 1.0)
+        return UIColor(named: "cardDivider")!
     }
 
     func darkened(by factor: CGFloat) -> UIColor {
