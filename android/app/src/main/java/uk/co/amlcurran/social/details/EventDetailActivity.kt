e7f2b64ff7d107c1dca15b3c8c5c1a09e4c9255a
amlcurran <aml.curran@gmail.com>
2025-04-26 11:55:42 +0100

    Move more event details into compose

--- android/app/src/main/java/uk/co/amlcurran/social/details/EventDetailActivity.kt
+++ android/app/src/main/java/uk/co/amlcurran/social/details/EventDetailActivity.kt
@@ -12,12 +12,22 @@ import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
@@ -39,6 +49,70 @@ import uk.co.amlcurran.social.R
 import uk.co.amlcurran.social.UserSettings
 import uk.co.amlcurran.social.databinding.ActivityEventDetailsBinding
 import kotlin.coroutines.suspendCoroutine
+import androidx.core.net.toUri
+
+private suspend fun findLocation(context: Context, location: String): LatLng? {
+    return suspendCoroutine { continuation ->
+        try {
+            val result = Geocoder(context).getFromLocationName(location, 10)?.firstOrNull()
+            val latLng = result?.let { LatLng(it.latitude, it.longitude) }
+            continuation.resumeWith(Result.success(latLng))
+        } catch (e: Throwable) {
+            continuation.resumeWith(Result.failure(e))
+        }
+    }
+}
+
+@Composable
+private fun EventMap(event: Event) {
+    val (location, setLocation) = remember { mutableStateOf<LatLng?>(null) }
+    val context = LocalContext.current
+    LaunchedEffect(event.location) {
+        event.location?.let {
+            launch(Dispatchers.IO) {
+                if (it.isNotBlank()) {
+                    findLocation(context, it)?.let { setLocation(it) }
+                }
+            }
+        }
+    }
+    if (location != null) {
+        GoogleMap(
+            modifier = Modifier
+                .height(200.dp)
+                .clip(
+                    RoundedCornerShape(
+                        topStart = 0.dp,
+                        topEnd = 0.dp,
+                        bottomStart = 8.dp,
+                        bottomEnd = 8.dp
+                    )
+                )
+                .offset(y = (-8).dp),
+            onMapClick = {
+                context.startActivity(Intent(Intent.ACTION_VIEW).apply {
+                    data =
+                        "https://www.google.com/maps/dir/?api=1&destination=${location.latitude},${location.longitude}".toUri()
+                })
+            },
+            uiSettings = MapUiSettings(
+                zoomControlsEnabled = false,
+                rotationGesturesEnabled = false,
+                scrollGesturesEnabled = false,
+                tiltGesturesEnabled = false,
+                zoomGesturesEnabled = false
+            ),
+            cameraPositionState = CameraPositionState(
+                CameraPosition.fromLatLngZoom(
+                    location,
+                    15f
+                )
+            )
+        ) {
+            Marker(state = MarkerState(position = location))
+        }
+    }
+}
 
 class EventDetailActivity : AppCompatActivity() {
 
@@ -91,70 +165,14 @@ class EventDetailActivity : AppCompatActivity() {
 
     private fun render(event: Event) {
         binding.eventCard2.setContent {
-            EventCard(event = event)
+            Column {
+                EventCard(event = event, modifier = Modifier.fillMaxWidth())
+                EventMap(event)
+            }
         }
         binding.detailToolbar.menu.findItem(R.id.menu_open_outside).isVisible = true
         binding.detailToolbar.menu.findItem(R.id.menu_delete_event).isVisible = true
         binding.detailToolbar.menu.findItem(R.id.menu_hide_event).isVisible = true
-        updateMap(event.location)
-    }
-
-    private fun updateMap(location: String?) {
-        location?.let {
-            lifecycleScope.launch(Dispatchers.IO) {
-                if (it.isNotBlank()) {
-                    findLocation(it)?.let { show(it) }
-                }
-            }
-        }
-    }
-
-    private fun show(latLng: LatLng) {
-        binding.mapHost.setContent {
-            GoogleMap(
-                modifier = Modifier.clip(
-                    RoundedCornerShape(
-                        topStart = 0.dp,
-                        topEnd = 0.dp,
-                        bottomStart = 8.dp,
-                        bottomEnd = 8.dp
-                    )
-                ),
-                onMapClick = {
-                    startActivity(Intent(Intent.ACTION_VIEW).apply {
-                        data =
-                            Uri.parse("https://www.google.com/maps/dir/?api=1&destination=${latLng.latitude},${latLng.longitude}")
-                    })
-                },
-                uiSettings = MapUiSettings(
-                    zoomControlsEnabled = false,
-                    rotationGesturesEnabled = false,
-                    scrollGesturesEnabled = false,
-                    tiltGesturesEnabled = false,
-                    zoomGesturesEnabled = false
-                ),
-                cameraPositionState = CameraPositionState(
-                    CameraPosition.fromLatLngZoom(
-                        latLng,
-                        15f
-                    )
-                )
-            ) {
-                Marker(state = MarkerState(position = latLng))
-            }
-        }
-    }
-
-    private suspend fun findLocation(location: String): LatLng? {
-        return suspendCoroutine { continuation ->
-            try {
-                val result = Geocoder(this).getFromLocationName(location, 10)?.firstOrNull()
-                val latLng = result?.let { LatLng(it.latitude, it.longitude) }
-                continuation.resumeWith(Result.success(latLng))
-            } catch (e: Throwable) {
-                continuation.resumeWith(Result.failure(e))
-            }
-        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
